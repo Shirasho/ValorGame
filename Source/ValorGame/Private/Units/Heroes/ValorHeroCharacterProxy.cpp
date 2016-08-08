a28e2211cca04f71b18671215005070ab886ddcd
Shirasho <ethan.treff@gmail.com>
2016-08-08 14:11:10 -0400

    VG-1 #resolve JIRA Smart Commit test 4

--- Source/ValorGame/Private/Units/Heroes/ValorHeroCharacterProxy.cpp
+++ Source/ValorGame/Private/Units/Heroes/ValorHeroCharacterProxy.cpp
@@ -21,6 +21,7 @@ AValorHeroCharacterProxy::AValorHeroCharacterProxy(const FObjectInitializer& Obj
 
 	bCenterCamera = false;
 
+
 	UCapsuleComponent* DummyRootComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("DummyRootComponent"));
 	DummyRootComponent->InitCapsuleSize(1.f, 1.f);
 	DummyRootComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
